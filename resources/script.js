257d06df18feef42e86b6333d9200108683ef620
chazthecoder <paytonchaz@gmail.com>
2025-02-19 18:10:37 -0500

    Completing About Page

--- resources/script.js
+++ resources/script.js
@@ -1,1 +1,25 @@
-let navigation = document.querySelector('nav');
+
+
+
+
+
+
+//let transitionedElement = document.createElement('navOverlay:active');
+let openNav = document.getElementById('openNav')
+let navigation = document.getElementById('navOverlay');
+let closeNav = document.getElementById('closeNav');
+
+function dropDownNav() {
+    navigation.style.zIndex = '1';
+
+}
+
+function reverseDropDownNav() {
+    navigation.style.zIndex = '0';
+
+}
+
+openNav.addEventListener('click', dropDownNav);
+closeNav.addEventListener('click', reverseDropDownNav);
+
+
